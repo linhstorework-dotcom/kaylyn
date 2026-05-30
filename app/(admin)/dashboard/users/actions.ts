1e2a54f8ac94f860164c22b25a175740b5ff0203
anhquantrieu01 <trieuanhquan.01@gmail.com>
2026-05-30 11:03:40 +0700

    fix

--- app/(admin)/dashboard/users/actions.ts
+++ app/(admin)/dashboard/users/actions.ts
@@ -10,14 +10,12 @@ export async function createUser(formData: FormData) {
 
   const name = formData.get("name") as string
   const email = formData.get("email") as string
-  const password = formData.get("password") as string
   const role = formData.get("role") as Role
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password,
       role,
     },
   })
